3b47e799ca8fa2f5a73c7b8eb774b8528a881d0c
Rodolfo Roman <rromanv@gmail.com>
2021-07-08 23:12:35 -0500

    Example Finished

---
+++ Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFExample2
+{
+    public partial class Blog
+    {
+        public Blog()
+        {
+            Posts = new HashSet<Post>();
+        }
+
+        public long BlogId { get; set; }
+        public string Url { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ blogContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EFExample2
+{
+    public partial class blogContext : DbContext
+    {
+        public blogContext()
+        {
+        }
+
+        public blogContext(DbContextOptions<blogContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Blog> Blogs { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlite("Data Source=blog.sqlite");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.HasIndex(e => e.BlogId, "IX_Posts_BlogId");
+
+                entity.HasOne(d => d.Blog)
+                    .WithMany(p => p.Posts)
+                    .HasForeignKey(d => d.BlogId);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
